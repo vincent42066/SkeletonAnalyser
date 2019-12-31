5eb6dc6dd6643ae5ab03e8977a4f91ddb2db21b8
Vincent Desnos <vincent.desnos@hotmail.fr>
2019-12-31 12:00:33 +0100

    start to work on examples

---
+++ NuitrackSDK/Examples/nuitrack_gl_sample/build/CMakeFiles/nuitrack_gl_sample.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nuitrack_gl_sample.dir/src/NuitrackGLSample.cpp.obj"
+  "CMakeFiles/nuitrack_gl_sample.dir/src/main.cpp.obj"
+  "libnuitrack_gl_sample.dll.a"
+  "nuitrack_gl_sample.exe"
+  "nuitrack_gl_sample.exe.manifest"
+  "nuitrack_gl_sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nuitrack_gl_sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ NuitrackSDK/Examples/nuitrack_ni_gl_sample/build/CMakeFiles/nuitrack_ni_gl_sample.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nuitrack_ni_gl_sample.dir/src/NiGLSample.cpp.obj"
+  "CMakeFiles/nuitrack_ni_gl_sample.dir/src/main.cpp.obj"
+  "libnuitrack_ni_gl_sample.dll.a"
+  "nuitrack_ni_gl_sample.exe"
+  "nuitrack_ni_gl_sample.exe.manifest"
+  "nuitrack_ni_gl_sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nuitrack_ni_gl_sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
